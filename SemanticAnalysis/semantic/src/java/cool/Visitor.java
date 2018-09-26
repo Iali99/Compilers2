03acd04f43e531e6ca80dd072f60d0729c4b88f4
sudo-chawhan <cs16btech11037@iith.ac.in>
2018-09-27 01:56:19 +0530

    add GlobalData

--- SemanticAnalysis/semantic/src/java/cool/Visitor.java
+++ SemanticAnalysis/semantic/src/java/cool/Visitor.java
@@ -4,17 +4,41 @@ public class Visitor{
 	public void visitorProgram(AST.program p){
 		List<AST.class_> classes = p.classes;
 		
+		// init classTable
+		GlobalData.classTable = new HashMap<String, String>;
+
 		// go to each class and add it to classTable
 		for(AST.class_ iter : classes){
-			if(classTable.containsKey(iter.name)){
-				giveError("class redefined : " + iter.name, iter.lineNo);
+			if(GlobalData.classTable.containsKey(iter.name)){
+				GlobalData.GiveError("class redefined : " + iter.name, iter.lineNo);
 			}
 			else
-				classTable.put(iter.name, "Object");
+				GlobalData.classTable.put(iter.name, "Object");
 		}
 
 		// iterate over each class and check for cycles
 		for(AST.class_ iter : classes){
+			String parent = iter.parent;
+			
+			if(parent == Object){
+				continue;
+			}
+			// check existence of parent
+			if(!GlobalData.classTable.containsKey(parent)){
+				GlobalData.GiveError("class does not exist : " + parent, iter.lineNo);
+			}
+			else{
+				// check for loops
+				String grandparent = GlobalData.classTable.get(parent);
+				while(grandparent != Object){
+					if(grandparent == iter.name){
+						GlobalData.GiveError("cycles detected", iter.lineNo);
+						return;
+					}
+					grandparent = GlobalData.classTable.get(grandparent);
+				}
+				GlobalData.classTable.put(iter.name, parent);
+			}
 
 		}
 
