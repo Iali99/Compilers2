a6df82abe5af98bbd5d68f4c0e18e5f685c55c2b
sudo-chawhan <cs16btech11037@iith.ac.in>
2018-10-08 23:45:00 +0530

    add visit methods and helper methods

--- SemanticAnalysis/semantic/src/java/cool/GlobalData.java
+++ SemanticAnalysis/semantic/src/java/cool/GlobalData.java
@@ -10,7 +10,7 @@ public class GlobalData{
         public static final String BOOL_TYPE = "Bool";
         public static final String STRING_TYPE = "String";
         public static final String MAIN_TYPE = "Main";
-        public static final bool is_standard(String name){
+        public static final boolean is_standard(String name){
             if(name.equals(IO_TYPE)) return true;
             if(name.equals(STRING_TYPE)) return true;
             if(name.equals(BOOL_TYPE)) return true;
@@ -21,12 +21,13 @@ public class GlobalData{
 
   // ClassTable - maps class name to class parent
   public static ScopeTable<String> attrScopeTable;
+  // function name -> FunctionTypeMangledName
   public static ScopeTable<String> methodScopeTable;
   public static HashMap<String, String> classTable;
   public static InheritanceGraph inheritanceGraph;
 
   //get mangled name of a function with classname and parameters
-  public static String mangledNameWithClassname(String classname, AST.method m)
+  public static String mangledNameWithClass(String classname, AST.method m)
   {
     StringBuilder mangledName = new StringBuilder();
     mangledName.append(classname).append("$");
@@ -43,7 +44,7 @@ public class GlobalData{
   }
 
   //get mangled name without classname and with parameters and its type
-  public static String mangledNameWithoutClassname(AST.method m)
+  public static String mangledNameWithType(AST.method m)
   {
     StringBuilder mangledName = new StringBuilder();
     mangledName.append(m.typeid).append("$");
--- SemanticAnalysis/semantic/src/java/cool/Visitor.java
+++ SemanticAnalysis/semantic/src/java/cool/Visitor.java
@@ -15,10 +15,9 @@ public class Visitor{
 	public void visit(AST.class_ c){
 
         Global.currentClass = c.name;
-
-		// checking all its features for semantics
+		// visiting all its features for semantics
         for(AST.feature f: c.features){
-            if(f instanceof AST.attr){ // Its a variable
+            if(f instanceof AST.attr){ // Its an attribute
                 AST.attr a = (AST.attr) f;
                 visit(a);
             }else{ // Its a method
@@ -26,16 +25,21 @@ public class Visitor{
                 visit(m);
             }
         }
+		// if this is main class
+		if(c.name.equals(GlobalData.Const.MAIN_TYPE)){
+			// main class
+			String mainMethod = GlobalData.methodScopeTable.lookUpLocal(c.name);
+			if(mainMethod==null){
+				// error main method missing
+			}else if(hasArguments(mainMethod)){ 
+				// error main has arguments
+			}
+		}
 
 		// visitor for class
 		if(GlobalData.is_standard(AST.class_.name)){
 			return;
 		}
-
-		// visiting all features
-        for(AST.feature f: c.features){
-            visit(f);
-        }
 	}
 
 	public void visit(AST.attr a){
@@ -52,11 +56,72 @@ public class Visitor{
 		}
 		if("self".equals(a.name)){
 			// error self keyword cant be used
-		} else if(!GlobalData.inheritanceGraph.con(a.typeid)){
-			// error 
+		}else if(!GlobalData.inheritanceGraph.containsClass(a.typeid)){
+			// error typeid does not exits
+			// todo recover
+		}else{
+			// attribute is valid
+			visit(a.value);
+			if(!(a.value instanceof AST.no_expr)){ // expression exists
+				// check if return type of a is same is expression return type
+				// keep in mind that return types can be instance of each other
+			}
 		}
-		if(GlobalData.inheritanceGraph(a.typeid))
-		
+	}
+
+	public void visit(AST.method m){
+		// check for undefined types
+		if(!GlobalData.InheritanceGraph.containsClass(m.typeid)){
+			// error type not defined
+			// recover
+		}
+		String globalMangledName;
+		// check if already defined locally
+		if(GlobalData.methodScopeTable.lookUpLocal(m.name) != null){
+			// error class redefined 
+		}
+		// check if the method exists globally
+		else if(globalMangledName = GlobalData.methodScopeTable.lookUpGlobal(m.name) && globalMangledName != null){
+			String thisTypeMangledName = mangledNameWithType(this);
+			if(!thisTypeMangledName.equals(globalMangledName)){
+				// error unmatched methods globally
+			}
+			GlobalData.methodScopeTable.insert(m.name, thisTypeMangledName);
+		}
+		// visit method internally
+		// enter scope
+		GlobalData.attrScopeTable.enterScope();
+
+		// loop through formal list
+		set<String> flist = new HashSet<>;
+		for(AST.formal fl : m.formals){
+			if(fl.name.equals("self")){
+				// error self cant be name
+			}else if(flist.contains(fl.name)){
+				// error formal redefined
+			}else{
+				flist.add(fl.name);
+			}
+			visit(fl);
+		}
+		// visit method body
+		visit(m);
+
+		// check conformance of method's return type and body return type
+
+		// exit scope
+		GlobalData.attrScopeTable.exitScope();
 
 	}
+
+	public void visit(AST.formal f){
+		// check for undefined type
+		if(GlobalData.inheritanceGraph.containsClass(f.typeid)){
+			// error undefined type
+		}
+		else{
+			GlobalData.attrScopeTable.insert(f.name, f.typeid);
+		}
+	}
+
 }
