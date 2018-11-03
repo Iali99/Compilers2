f4e25630feb3b20f9d8d250d102afe7f5a285f98
Irfan <CS16BTECH11019@iith.ac.in>
2018-11-03 20:24:32 +0530

    mangledName function added

--- codegen/src/java/cool/GlobalData.java
+++ codegen/src/java/cool/GlobalData.java
@@ -6,4 +6,13 @@ public class GlobalData{
   public static PrintWriter out;
   //Counter for register variables
   public static int Counter;
+
+  public static String mangledName(String classname, AST.method m)
+  {
+    StringBuilder mangledName = new StringBuilder();
+    mangledName.append(classname).append("$");
+    mangledName.append(m.name).append("$");
+    return mangledName.toString();
+  }
+
 }
